828f3490f613bd7ae8ba079933a2d727b1583830
Hao Song <haosong.pitt@gmail.com>
2022-02-14 18:10:28 -0600

    20220214

---
+++ solutions/0100-0149/0136_Single_Number/solutions.py
@@ -0,0 +1,10 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        
+        ans = nums[0]
+        
+        
+        for num in nums[1:]:
+            ans ^= num
+            
+        return ans
