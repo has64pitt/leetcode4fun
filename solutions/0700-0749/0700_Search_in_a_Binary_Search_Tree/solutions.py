82208a6ca4cb86ffd581d7224db1497ed2545c18
Hao Song <haosong.pitt@gmail.com>
2022-04-13 19:32:56 -0500

    20220413

---
+++ solutions/0700-0749/0700_Search_in_a_Binary_Search_Tree/solutions.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        
+        
+        def go(node):
+            if not node:
+                return None
+            
+            if node.val == val:
+                return node
+            elif node.val > val:
+                return go(node.left)
+            else:
+                return go(node.right)
+            
+        return go(root)
