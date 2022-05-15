f725c09cfb5ca38e8729378a83c2e46fb4b8fc33
Hao Song <haosong.pitt@gmail.com>
2022-05-14 21:19:44 -0500

    20220514

---
+++ solutions/1300-1349/1302_Deepest_Leave_Sum/solutions.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def deepestLeavesSum(self, root: Optional[TreeNode]) -> int:
+        
+        
+        queue = collections.deque([root])
+        
+        ans = 0
+        while queue:
+            qlen = len(queue)
+            ans = 0
+            for _ in range(qlen):
+                node = queue.popleft()
+                ans += node.val
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+        
+        return ans
